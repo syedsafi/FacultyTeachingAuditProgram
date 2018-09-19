9a4d0eed28716993453534652fa195ee3c23b65e
safi <safi.ullah50@hotmail.com>
2018-09-19 14:46:52 -0500

    added btn wil

--- FacultyTeachingAuditProgram/Form1.Designer.cs
+++ FacultyTeachingAuditProgram/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.lblselectfile = new System.Windows.Forms.Label();
             this.lblnamedisplay = new System.Windows.Forms.Label();
             this.btnimport = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnbrowse
@@ -83,11 +84,22 @@
             this.btnimport.UseVisualStyleBackColor = true;
             this.btnimport.Click += new System.EventHandler(this.btnimport_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(140, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "wil";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(453, 372);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnimport);
             this.Controls.Add(this.lblnamedisplay);
             this.Controls.Add(this.lblselectfile);
@@ -107,6 +119,7 @@
         private System.Windows.Forms.Label lblselectfile;
         private System.Windows.Forms.Label lblnamedisplay;
         private System.Windows.Forms.Button btnimport;
+        private System.Windows.Forms.Button button1;
     }
 }
 
